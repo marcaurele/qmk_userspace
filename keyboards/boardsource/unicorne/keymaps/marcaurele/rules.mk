6f00858e1e596658699af123c9551139e8dd9a39
Marc-Aurèle Brothier <m@brothier.org>
2026-04-25 14:29:53 +0200

    fix: possible kb sleep issue correction

--- keyboards/boardsource/unicorne/keymaps/marcaurele/rules.mk
+++ keyboards/boardsource/unicorne/keymaps/marcaurele/rules.mk
@@ -7,6 +7,10 @@ ENCODER_ENABLE = no
 POINTING_DEVICE_ENABLE = no
 ANALOG_JOYSTICK_ENABLE = no
 
+# Test for resume: https://docs.qmk.fm/faq_misc#problem-in-bios-uefi-setup-resume-sleep-wake-power-cycles
+#NKRO_ENABLE = no
+SLEEP_LED_ENABLE = no
+
 # Ensure features are activated
 LTO_ENABLE = yes
 OLED_ENABLE = yes
